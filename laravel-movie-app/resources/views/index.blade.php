7bcc8137dcfbd1db338ccbd007485b5f252d9e34
Gullian <gullianvdwalt1@gmail.com>
2020-12-15 08:07:24 +0200

    init

---
+++ laravel-movie-app/resources/views/index.blade.php
@@ -0,0 +1,402 @@
+@extends('layouts.app')
+
+@section('content')
+{{-- Trending Movies --}}
+<div class="container mx-auto px-4 pt-16">
+    <div class="popular-movies">
+        <h2 class="uppercase tracking-wider text-gray-600 text-lg font-semibold">Trending Movies</h2>
+        <div class="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+            <div class="mt-8">
+                {{-- Poster --}}
+                <a href="#">
+                    <img src="/images/parasite.jpg" alt="Parasite" class="hover:opacity-75 transition ease-in-out duration-150">
+                </a>
+                {{-- Details --}}
+                <div class="mt-2">
+                    <a href="#" class="text-lg mt-2 hover:text-gray-700">Parasite</a>
+                    <div class="flex items-center text-gray-600 mt-1">
+                        <span>star</span>
+                        <span class="ml-1">85%</span>
+                        <span class="mx-2">|</span>
+                        <span>release date</span>
+                    </div>
+                    <div class="text-gray-600 text-sm">
+                        Action, Thriller, Comedy
+                    </div>
+                </div>
+            </div>
+            <div class="mt-8">
+                {{-- Poster --}}
+                <a href="#">
+                    <img src="/images/parasite.jpg" alt="Parasite" class="hover:opacity-75 transition ease-in-out duration-150">
+                </a>
+                {{-- Details --}}
+                <div class="mt-2">
+                    <a href="#" class="text-lg mt-2 hover:text-gray-700">Parasite</a>
+                    <div class="flex items-center text-gray-600 mt-1">
+                        <span>star</span>
+                        <span class="ml-1">85%</span>
+                        <span class="mx-2">|</span>
+                        <span>release date</span>
+                    </div>
+                    <div class="text-gray-600 text-sm">
+                        Action, Thriller, Comedy
+                    </div>
+                </div>
+            </div>
+            <div class="mt-8">
+                {{-- Poster --}}
+                <a href="#">
+                    <img src="/images/parasite.jpg" alt="Parasite" class="hover:opacity-75 transition ease-in-out duration-150">
+                </a>
+                {{-- Details --}}
+                <div class="mt-2">
+                    <a href="#" class="text-lg mt-2 hover:text-gray-700">Parasite</a>
+                    <div class="flex items-center text-gray-600 mt-1">
+                        <span>star</span>
+                        <span class="ml-1">85%</span>
+                        <span class="mx-2">|</span>
+                        <span>release date</span>
+                    </div>
+                    <div class="text-gray-600 text-sm">
+                        Action, Thriller, Comedy
+                    </div>
+                </div>
+            </div>
+            <div class="mt-8">
+                {{-- Poster --}}
+                <a href="#">
+                    <img src="/images/parasite.jpg" alt="Parasite" class="hover:opacity-75 transition ease-in-out duration-150">
+                </a>
+                {{-- Details --}}
+                <div class="mt-2">
+                    <a href="#" class="text-lg mt-2 hover:text-gray-700">Parasite</a>
+                    <div class="flex items-center text-gray-600 mt-1">
+                        <span>star</span>
+                        <span class="ml-1">85%</span>
+                        <span class="mx-2">|</span>
+                        <span>release date</span>
+                    </div>
+                    <div class="text-gray-600 text-sm">
+                        Action, Thriller, Comedy
+                    </div>
+                </div>
+            </div>
+            <div class="mt-8">
+                {{-- Poster --}}
+                <a href="#">
+                    <img src="/images/parasite.jpg" alt="Parasite" class="hover:opacity-75 transition ease-in-out duration-150">
+                </a>
+                {{-- Details --}}
+                <div class="mt-2">
+                    <a href="#" class="text-lg mt-2 hover:text-gray-700">Parasite</a>
+                    <div class="flex items-center text-gray-600 mt-1">
+                        <span>star</span>
+                        <span class="ml-1">85%</span>
+                        <span class="mx-2">|</span>
+                        <span>release date</span>
+                    </div>
+                    <div class="text-gray-600 text-sm">
+                        Action, Thriller, Comedy
+                    </div>
+                </div>
+            </div>
+            <div class="mt-8">
+                {{-- Poster --}}
+                <a href="#">
+                    <img src="/images/parasite.jpg" alt="Parasite" class="hover:opacity-75 transition ease-in-out duration-150">
+                </a>
+                {{-- Details --}}
+                <div class="mt-2">
+                    <a href="#" class="text-lg mt-2 hover:text-gray-700">Parasite</a>
+                    <div class="flex items-center text-gray-600 mt-1">
+                        <span>star</span>
+                        <span class="ml-1">85%</span>
+                        <span class="mx-2">|</span>
+                        <span>release date</span>
+                    </div>
+                    <div class="text-gray-600 text-sm">
+                        Action, Thriller, Comedy
+                    </div>
+                </div>
+            </div>
+            <div class="mt-8">
+                {{-- Poster --}}
+                <a href="#">
+                    <img src="/images/parasite.jpg" alt="Parasite" class="hover:opacity-75 transition ease-in-out duration-150">
+                </a>
+                {{-- Details --}}
+                <div class="mt-2">
+                    <a href="#" class="text-lg mt-2 hover:text-gray-700">Parasite</a>
+                    <div class="flex items-center text-gray-600 mt-1">
+                        <span>star</span>
+                        <span class="ml-1">85%</span>
+                        <span class="mx-2">|</span>
+                        <span>release date</span>
+                    </div>
+                    <div class="text-gray-600 text-sm">
+                        Action, Thriller, Comedy
+                    </div>
+                </div>
+            </div>
+            <div class="mt-8">
+                {{-- Poster --}}
+                <a href="#">
+                    <img src="/images/parasite.jpg" alt="Parasite" class="hover:opacity-75 transition ease-in-out duration-150">
+                </a>
+                {{-- Details --}}
+                <div class="mt-2">
+                    <a href="#" class="text-lg mt-2 hover:text-gray-700">Parasite</a>
+                    <div class="flex items-center text-gray-600 mt-1">
+                        <span>star</span>
+                        <span class="ml-1">85%</span>
+                        <span class="mx-2">|</span>
+                        <span>release date</span>
+                    </div>
+                    <div class="text-gray-600 text-sm">
+                        Action, Thriller, Comedy
+                    </div>
+                </div>
+            </div>
+            <div class="mt-8">
+                {{-- Poster --}}
+                <a href="#">
+                    <img src="/images/parasite.jpg" alt="Parasite" class="hover:opacity-75 transition ease-in-out duration-150">
+                </a>
+                {{-- Details --}}
+                <div class="mt-2">
+                    <a href="#" class="text-lg mt-2 hover:text-gray-700">Parasite</a>
+                    <div class="flex items-center text-gray-600 mt-1">
+                        <span>star</span>
+                        <span class="ml-1">85%</span>
+                        <span class="mx-2">|</span>
+                        <span>release date</span>
+                    </div>
+                    <div class="text-gray-600 text-sm">
+                        Action, Thriller, Comedy
+                    </div>
+                </div>
+            </div>
+            <div class="mt-8">
+                {{-- Poster --}}
+                <a href="#">
+                    <img src="/images/parasite.jpg" alt="Parasite" class="hover:opacity-75 transition ease-in-out duration-150">
+                </a>
+                {{-- Details --}}
+                <div class="mt-2">
+                    <a href="#" class="text-lg mt-2 hover:text-gray-700">Parasite</a>
+                    <div class="flex items-center text-gray-600 mt-1">
+                        <span>star</span>
+                        <span class="ml-1">85%</span>
+                        <span class="mx-2">|</span>
+                        <span>release date</span>
+                    </div>
+                    <div class="text-gray-600 text-sm">
+                        Action, Thriller, Comedy
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+{{-- Now Playing --}}
+<div class="container mx-auto px-4 pt-16 mt-4">
+    <div class="popular-movies">
+        <h2 class="uppercase tracking-wider text-gray-600 text-lg font-semibold">Now Playing</h2>
+        <div class="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+            <div class="mt-8">
+                {{-- Poster --}}
+                <a href="#">
+                    <img src="/images/parasite.jpg" alt="Parasite" class="hover:opacity-75 transition ease-in-out duration-150">
+                </a>
+                {{-- Details --}}
+                <div class="mt-2">
+                    <a href="#" class="text-lg mt-2 hover:text-gray-700">Parasite</a>
+                    <div class="flex items-center text-gray-600 mt-1">
+                        <span>star</span>
+                        <span class="ml-1">85%</span>
+                        <span class="mx-2">|</span>
+                        <span>release date</span>
+                    </div>
+                    <div class="text-gray-600 text-sm">
+                        Action, Thriller, Comedy
+                    </div>
+                </div>
+            </div>
+            <div class="mt-8">
+                {{-- Poster --}}
+                <a href="#">
+                    <img src="/images/parasite.jpg" alt="Parasite" class="hover:opacity-75 transition ease-in-out duration-150">
+                </a>
+                {{-- Details --}}
+                <div class="mt-2">
+                    <a href="#" class="text-lg mt-2 hover:text-gray-700">Parasite</a>
+                    <div class="flex items-center text-gray-600 mt-1">
+                        <span>star</span>
+                        <span class="ml-1">85%</span>
+                        <span class="mx-2">|</span>
+                        <span>release date</span>
+                    </div>
+                    <div class="text-gray-600 text-sm">
+                        Action, Thriller, Comedy
+                    </div>
+                </div>
+            </div>
+            <div class="mt-8">
+                {{-- Poster --}}
+                <a href="#">
+                    <img src="/images/parasite.jpg" alt="Parasite" class="hover:opacity-75 transition ease-in-out duration-150">
+                </a>
+                {{-- Details --}}
+                <div class="mt-2">
+                    <a href="#" class="text-lg mt-2 hover:text-gray-700">Parasite</a>
+                    <div class="flex items-center text-gray-600 mt-1">
+                        <span>star</span>
+                        <span class="ml-1">85%</span>
+                        <span class="mx-2">|</span>
+                        <span>release date</span>
+                    </div>
+                    <div class="text-gray-600 text-sm">
+                        Action, Thriller, Comedy
+                    </div>
+                </div>
+            </div>
+            <div class="mt-8">
+                {{-- Poster --}}
+                <a href="#">
+                    <img src="/images/parasite.jpg" alt="Parasite" class="hover:opacity-75 transition ease-in-out duration-150">
+                </a>
+                {{-- Details --}}
+                <div class="mt-2">
+                    <a href="#" class="text-lg mt-2 hover:text-gray-700">Parasite</a>
+                    <div class="flex items-center text-gray-600 mt-1">
+                        <span>star</span>
+                        <span class="ml-1">85%</span>
+                        <span class="mx-2">|</span>
+                        <span>release date</span>
+                    </div>
+                    <div class="text-gray-600 text-sm">
+                        Action, Thriller, Comedy
+                    </div>
+                </div>
+            </div>
+            <div class="mt-8">
+                {{-- Poster --}}
+                <a href="#">
+                    <img src="/images/parasite.jpg" alt="Parasite" class="hover:opacity-75 transition ease-in-out duration-150">
+                </a>
+                {{-- Details --}}
+                <div class="mt-2">
+                    <a href="#" class="text-lg mt-2 hover:text-gray-700">Parasite</a>
+                    <div class="flex items-center text-gray-600 mt-1">
+                        <span>star</span>
+                        <span class="ml-1">85%</span>
+                        <span class="mx-2">|</span>
+                        <span>release date</span>
+                    </div>
+                    <div class="text-gray-600 text-sm">
+                        Action, Thriller, Comedy
+                    </div>
+                </div>
+            </div>
+            <div class="mt-8">
+                {{-- Poster --}}
+                <a href="#">
+                    <img src="/images/parasite.jpg" alt="Parasite" class="hover:opacity-75 transition ease-in-out duration-150">
+                </a>
+                {{-- Details --}}
+                <div class="mt-2">
+                    <a href="#" class="text-lg mt-2 hover:text-gray-700">Parasite</a>
+                    <div class="flex items-center text-gray-600 mt-1">
+                        <span>star</span>
+                        <span class="ml-1">85%</span>
+                        <span class="mx-2">|</span>
+                        <span>release date</span>
+                    </div>
+                    <div class="text-gray-600 text-sm">
+                        Action, Thriller, Comedy
+                    </div>
+                </div>
+            </div>
+            <div class="mt-8">
+                {{-- Poster --}}
+                <a href="#">
+                    <img src="/images/parasite.jpg" alt="Parasite" class="hover:opacity-75 transition ease-in-out duration-150">
+                </a>
+                {{-- Details --}}
+                <div class="mt-2">
+                    <a href="#" class="text-lg mt-2 hover:text-gray-700">Parasite</a>
+                    <div class="flex items-center text-gray-600 mt-1">
+                        <span>star</span>
+                        <span class="ml-1">85%</span>
+                        <span class="mx-2">|</span>
+                        <span>release date</span>
+                    </div>
+                    <div class="text-gray-600 text-sm">
+                        Action, Thriller, Comedy
+                    </div>
+                </div>
+            </div>
+            <div class="mt-8">
+                {{-- Poster --}}
+                <a href="#">
+                    <img src="/images/parasite.jpg" alt="Parasite" class="hover:opacity-75 transition ease-in-out duration-150">
+                </a>
+                {{-- Details --}}
+                <div class="mt-2">
+                    <a href="#" class="text-lg mt-2 hover:text-gray-700">Parasite</a>
+                    <div class="flex items-center text-gray-600 mt-1">
+                        <span>star</span>
+                        <span class="ml-1">85%</span>
+                        <span class="mx-2">|</span>
+                        <span>release date</span>
+                    </div>
+                    <div class="text-gray-600 text-sm">
+                        Action, Thriller, Comedy
+                    </div>
+                </div>
+            </div>
+            <div class="mt-8">
+                {{-- Poster --}}
+                <a href="#">
+                    <img src="/images/parasite.jpg" alt="Parasite" class="hover:opacity-75 transition ease-in-out duration-150">
+                </a>
+                {{-- Details --}}
+                <div class="mt-2">
+                    <a href="#" class="text-lg mt-2 hover:text-gray-700">Parasite</a>
+                    <div class="flex items-center text-gray-600 mt-1">
+                        <span>star</span>
+                        <span class="ml-1">85%</span>
+                        <span class="mx-2">|</span>
+                        <span>release date</span>
+                    </div>
+                    <div class="text-gray-600 text-sm">
+                        Action, Thriller, Comedy
+                    </div>
+                </div>
+            </div>
+            <div class="mt-8">
+                {{-- Poster --}}
+                <a href="#">
+                    <img src="/images/parasite.jpg" alt="Parasite" class="hover:opacity-75 transition ease-in-out duration-150">
+                </a>
+                {{-- Details --}}
+                <div class="mt-2">
+                    <a href="#" class="text-lg mt-2 hover:text-gray-700">Parasite</a>
+                    <div class="flex items-center text-gray-600 mt-1">
+                        <span>star</span>
+                        <span class="ml-1">85%</span>
+                        <span class="mx-2">|</span>
+                        <span>release date</span>
+                    </div>
+                    <div class="text-gray-600 text-sm">
+                        Action, Thriller, Comedy
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
