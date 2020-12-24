006cf07667d439adef649370ea04b22d0883c008
Gullian <gullianvdwalt1@gmail.com>
2020-12-24 06:51:33 +0200

    Configured routes for index and show

--- laravel-movie-app/routes/web.php
+++ laravel-movie-app/routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\MoviesController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/','index');
-Route::view('/movie','show');
+
+// Get Movies
+Route::get('/', [MoviesController::class, 'index']);
+// Get Movie
+//Route::get('/movies/{id}', [MoviesController::class, 'show']);
+Route::get('/movies/{id}', 'MoviesController@show')->name('movies.show');
