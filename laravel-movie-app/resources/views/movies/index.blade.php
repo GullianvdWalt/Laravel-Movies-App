1e7f27f808dbde8e518117366111ef448be5557c
Gullian <gullianvdwalt1@gmail.com>
2020-12-24 06:52:23 +0200

    moved view to folder, view updates

---
+++ laravel-movie-app/resources/views/movies/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+
+{{-- Trending Movies --}}
+<div class="container mx-auto px-4 pt-16">
+    <div class="popular-movies">
+        <h2 class="uppercase tracking-wider text-gray-600 text-lg font-semibold">Trending Movies</h2>
+        {{-- Popular/Trending Movies Main Container --}}
+        <div class="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+            {{-- Popular/Trending Movie Single container --}}
+                {{-- Dynamic Content From TMDB API --}}
+            @foreach ($popularMovies as $movie)
+                <x-movie-card :movie="$movie" :genres="$genres"/>
+            @endforeach
+        </div>
+    </div>
+</div>
+
+{{-- Now Playing --}}
+<div class="container mx-auto px-4 pt-16 mt-4">
+    <div class="popular-movies">
+        <h2 class="uppercase tracking-wider text-gray-600 text-lg font-semibold">Now Playing</h2>
+        {{-- Now Playing Movies Main Container --}}
+        <div class="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+            {{-- Now Playing Movie Single container --}}
+                {{-- Dynamic Content From TMDB API --}}
+                @foreach ($nowPlayingMovies as $movie)
+                    <x-movie-card :movie="$movie" :genres="$genres"/>
+                @endforeach
+        </div>
+    </div>
+</div>
+
+@endsection
