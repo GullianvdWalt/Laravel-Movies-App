da4b0976adc211ba5add28d79e72b46a8a44bbbd
Gullian <gullianvdwalt1@gmail.com>
2020-12-30 10:45:18 +0200

    update

--- public/index.php
+++ public/index.php
@@ -35,6 +35,19 @@ require __DIR__.'/../vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
+| Turn On The Lights
+|--------------------------------------------------------------------------
+|
+| We need to illuminate PHP development, so let us turn on the lights.
+| This bootstraps the framework and gets it ready for use, then it
+| will load up this application so that we can run it and send
+| the responses back to the browser and delight our users.
+|
+*/
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+/*
+|--------------------------------------------------------------------------
 | Run The Application
 |--------------------------------------------------------------------------
 |
@@ -44,12 +57,22 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
-$kernel = $app->make(Kernel::class);
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
 
-$response = tap($kernel->handle(
-    $request = Request::capture()
-))->send();
+$response->send();
 
 $kernel->terminate($request, $response);
+
+// $app = require_once __DIR__.'/../bootstrap/app.php';
+
+// $kernel = $app->make(Kernel::class);
+
+// $response = tap($kernel->handle(
+//     $request = Request::capture()
+// ))->send();
+
+// $kernel->terminate($request, $response);
